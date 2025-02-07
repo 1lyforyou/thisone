de5a453d9faef949bce09ac14aed6b42591e0279
1lyforyou <maxon8085@gmail.com>
2025-02-07 17:58:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Data for each day
 const dayData = {
     7: {
-        date: "<button class="">Feb 07</button>",
+        date: "<button class='date-btn'>Feb 07</button>",
         message: "Happy Rose Day Maya!",
         gif: "https://static.toiimg.com/thumb/imgsize-23456,msid-67868345,width-600,resizemode-4/67868345.jpg",
         line: "A rose for the most special person in my life. 🌹",
@@ -9,6 +9,7 @@ const dayData = {
         bestNepaliLine: "यो गुलाब जस्तै, तिम्रो माया मेरो जीवन सुगन्ध र सौन्दर्यले भरिन्छ। ❤️"
     },
     8: {
+        date: "<button class='date-btn'>Feb 08</button>",
         message: "Happy Propose Day Maya!",
         gif: "https://st1.latestly.com/wp-content/uploads/2022/02/Happy-Propose-Day-gif_1.gif",
         line: "Will you be mine forever? 💍",
@@ -16,6 +17,7 @@ const dayData = {
         bestNepaliLine: "तिमी बिना मेरो जीवन अधुरो छ, के तिमी मेरो सधैंको साथ हुनेछौ? 💕"
     },
     9: {
+        date: "<button class='date-btn'>Feb 09</button>",
         message: "Happy Chocolate Day Maya!",
         gif: "https://i.pinimg.com/originals/49/05/81/490581327ce84d5e677bf4dac6234002.gif",
         line: "Sweet like you, my love. 🍫",
@@ -23,6 +25,7 @@ const dayData = {
         bestNepaliLine: "तिमी चकलेट जस्तै मिठो छौ, मेरो जीवनको सबैभन्दा प्यारो हिस्सा। 🍫❤️"
     },
     10: {
+        date: "<button class='date-btn'>Feb 10</button>",
         message: "Happy Teddy Day Maya!",
         gif: "https://static.india.com/wp-content/uploads/2016/02/teddy-day1.gif",
         line: "A cuddly teddy for my cuddly partner. 🧸",
@@ -30,6 +33,7 @@ const dayData = {
         bestNepaliLine: "यदि सम्भव छ भने, हरेक दिन म यो टेडी बियर जस्तै तिमीलाई कसिलो अँगालो मार्न चाहन्छु। 🤗"
     },
     11: {
+        date: "<button class='date-btn'>Feb 11</button>",
         message: "Happy Promise Day Maya!",
         gif: "https://media.tenor.com/WT4J27Vw0GgAAAAM/promise-sumitjaat.gif",
         line: "I promise to always be by your side. 🤝",
@@ -37,6 +41,7 @@ const dayData = {
         bestNepaliLine: "जुनसुकै परिस्थिति आए पनि, म तिमीलाई सधैं प्रेम गर्ने वाचा गर्छु। ❤️"
     },
     12: {
+        date: "<button class='date-btn'>Feb 12</button>",
         message: "Happy Hug Day Maya!",
         gif: "https://i.123g.us/c/efeb_hugdayfeb/card/337437.gif",
         line: "A warm hug for my love. 🤗",
@@ -44,6 +49,7 @@ const dayData = {
         bestNepaliLine: "मेरो यो अँगालो भित्र सधैं सुरक्षित रहनु। 🤗💕"
     },
     13: {
+        date: "<button class='date-btn'>Feb 13</button>",
         message: "Happy Kiss Day Maya!",
         gif: "https://i.pinimg.com/originals/b2/80/af/b280afed594fe394122b2ead6b5f5df8.gif",
         line: "Sending you a thousand kisses. 💋",
@@ -51,6 +57,7 @@ const dayData = {
         bestNepaliLine: "तिम्रो एक चुम्बन मेरो लागि हजार शब्द भन्दा गहिरो अर्थ राख्छ। 💋❤️"
     },
     14: {
+        date: "<button class='date-btn'>Feb 14</button>", 
         message: "Happy Valentine's Day Maya!",
         gif: "https://i.pinimg.com/originals/1d/de/6f/1dde6fbfe54010cdc054ec47998311ae.gif",
         line: "You are my forever Valentine. ❤️",
@@ -58,6 +65,7 @@ const dayData = {
         bestNepaliLine: "तिमी मेरो जीवनको सबैभन्दा सुन्दर निर्णय हौ, मेरो सधैंको भ्यालेन्टाइन! ❤️"
     },
     15: {
+        date: "<button class='date-btn'>Feb 15</button>",
         message: "Happy Slap Day Maya!",
         gif: "https://media.tenor.com/wDu33wHVM_wAAAAM/blu-zushi-black-and-white.gif",
         line: "Just a playful slap! 😜",
@@ -65,6 +73,7 @@ const dayData = {
         bestNepaliLine: "मजाकमा एउटा प्यारो थप्पड! 😜 तर मन सधैं तिमीलाई प्रेम गर्छ। ❤️"
     },
     16: {
+        date: "<button class='date-btn'>Feb 16</button>", 
         message: "Happy Kick Day Maya!",
         gif: "https://media.tenor.com/5iVv64OjO28AAAAM/milk-and-mocha-bear-couple.gif",
         line: "Kicking away all the negativity! 🦵",
@@ -72,6 +81,7 @@ const dayData = {
         bestNepaliLine: "माया कहिल्यै कम नहोस् भनेर एक प्यारा लात! 🦵😜"
     },
     17: {
+        date: "<button class='date-btn'>Feb 17</button>",
         message: "Happy Perfume Day Maya!",
         gif: "https://st1.latestly.com/wp-content/uploads/2024/02/Happy-Perfume-Day-380x214.jpg",
         line: "A fragrance as sweet as you. 🌸",
@@ -79,6 +89,7 @@ const dayData = {
         bestNepaliLine: "तिम्रो माया मेरो लागि सबैभन्दा मीठो सुगन्ध हो। 🌸❤️"
     },
     18: {
+        date: "<button class='date-btn'>Feb 18</button>",
         message: "Happy Flirt Day Maya!",
         gif: "https://i.pinimg.com/originals/f7/e3/f9/f7e3f9af60e53ca1976f35c18777182b.gif",
         line: "Flirting with you is my favorite hobby. 😘",
@@ -86,6 +97,7 @@ const dayData = {
         bestNepaliLine: "तिमीलाई जिस्क्याउनु मेरो मनपर्ने बानी हो, किनकि तिमी मेरो आत्मा साथी हौ। 😉💕"
     },
     19: {
+        date: "<button class='date-btn'>Feb 19</button>", 
         message: "Happy Confession Day Maya!",
         gif: "https://st1.latestly.com/wp-content/uploads/2024/02/Confession-Day.jpg",
         line: "I confess, I’m crazy about you. 💌",
@@ -93,6 +105,7 @@ const dayData = {
         bestNepaliLine: "सबैभन्दा ठूलो सत्य के हो भने, म तिमीलाई शब्दले भन्दा बढी माया गर्छु। 💖"
     },
     20: {
+        date: "<button class='date-btn'>Feb 20</button>", 
         message: "Happy Missing Day Maya!",
         gif: "https://www.hindustantimes.com/ht-img/img/2023/02/19/1600x900/Lead_1676777809256_1676777830869_1676777830869.jpg",
         line: "I miss you more than words can say. 💔",
@@ -100,6 +113,7 @@ const dayData = {
         bestNepaliLine: "तिमी जहाँ भए पनि, मेरो मुटु सधैं तिमीलाई खोज्छ। 💔"
     },
     21: {
+        date: "<button class='date-btn'>Feb 21</button>",
         message: "Happy Broken Day Maya!",
         gif: "https://st1.latestly.com/wp-content/uploads/2023/02/15-Happy-Breakup-Day-380x214.jpg",
         line: "Even broken, my heart beats for you. 💔",
